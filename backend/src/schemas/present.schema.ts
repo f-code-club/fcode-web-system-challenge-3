4edd4ab46c6e0bba047573e4f7c51bfe03b51b5d
hoangtuanqn <phamhoangtuanqn@gmail.com>
2026-01-12 09:16:48 +0700

    feat: update Present model and schema to include createdAt and updatedAt fields

--- backend/src/schemas/present.schema.ts
+++ backend/src/schemas/present.schema.ts
@@ -1,16 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-// model Present {
-//   id                 String   @id @default(uuid()) @db.VarChar(36)
-//   teamId             String   @map("team_id") @db.VarChar(36)
-//   tentativeSchedule  String   @map("tentative_schedule") @db.Json
-//   finalSchedule      String   @map("final_schedule") @db.Json
-//   createdAt          DateTime @default(now()) @map("created_at")
-//   updatedAt          DateTime @updatedAt @map("updated_at")
 
-//   team Team @relation(fields: [teamId], references: [id])
-
-//   @@map("presents")
-// }
 interface PresentType {
     id?: string;
     teamId: string;
